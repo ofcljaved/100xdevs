bd1584710664663a462f9135f71c72eb1fcf188a
javed <javed@local.com>
2024-06-05 18:23:43 +0530

    fix: sign-in to left side

--- src/components/OnBoardFooter.tsx
+++ src/components/OnBoardFooter.tsx
@@ -19,7 +19,7 @@ export const OnBoardFooter = ({
         <AnimatedBtn
           entering={FadeInLeft.duration(150)}
           exiting={FadeOutLeft.duration(150)}
-          className="self-start justify-self-end px-10 pb-14"
+          className=" px-10 pb-14"
           onPress={handleSkip}>
           <Text className="text-slate-600">Skip</Text>
         </AnimatedBtn>
@@ -28,7 +28,7 @@ export const OnBoardFooter = ({
         <AnimatedBtn
           entering={FadeInRight.duration(150)}
           exiting={FadeOutRight.duration(150)}
-          className="self-end justify-self-end px-10 pb-14"
+          className="ml-auto px-10 pb-14"
           onPress={handleSkip}>
           <Animated.Text className="text-slate-600">Sign In</Animated.Text>
         </AnimatedBtn>
